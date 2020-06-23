7f6bff82a25132ecb2f7d9eae099606948653806
송지필 <sjp@songjipil-ui-MacBook-Pro.local>
2020-06-23 10:24:20 +0900

    Updates

---
+++ precache-manifest.a7219cc61133fab815a8eed69d3df71b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0e957d09125cd3646939aba9d07b2e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "40ea6a8e7efb8479f205",
+    "url": "/movie_app_2020/static/css/main.63eb7561.chunk.css"
+  },
+  {
+    "revision": "47b962a428ac51b3826b",
+    "url": "/movie_app_2020/static/js/2.30990c06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.30990c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ea6a8e7efb8479f205",
+    "url": "/movie_app_2020/static/js/main.052d7c91.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
